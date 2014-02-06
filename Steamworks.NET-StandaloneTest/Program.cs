6e15af1c2388bdf05da469ea22287696ede46aba
Riley Labrecque <git@rileylabrecque.com>
2014-02-06 17:23:01 -0600

    Basic test to show file structure, Windows only currently

---
+++ Steamworks.NET-StandaloneTest/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using Steamworks;
+
+namespace SteamworksNET_StandaloneTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool ret = SteamAPI.Init();
+            if(!ret)
+            {
+                Console.WriteLine("SteamAPI.Init() failed!");
+                return;
+            }
+
+            ret = Packsize.Test();
+            Console.WriteLine("Packsize.Test() returned: " + ret);
+
+            while (!Console.KeyAvailable)
+            {
+                SteamAPI.RunCallbacks();
+                Thread.Sleep(100);
+            }
+
+            SteamAPI.Shutdown();
+        }
+    }
+}
