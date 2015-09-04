da4dd5866869a212929b6b8516ac48be2bab7b1d
Riley Labrecque <git@rileylabrecque.com>
2015-09-04 09:55:57 -0600

    General cleanup to the code

--- Steamworks.NET-StandaloneTest/Program.cs
+++ Steamworks.NET-StandaloneTest/Program.cs
@@ -7,14 +7,30 @@ using Steamworks;
 
 namespace SteamworksNET_StandaloneTest {
 	class Program {
-		static CallResult<NumberOfCurrentPlayers_t> NumberOfCurrentPlayers = CallResult<NumberOfCurrentPlayers_t>.Create(OnNumberOfCurrentPlayers);
+		static CallResult<NumberOfCurrentPlayers_t> m_NumberOfCurrentPlayers;
 		static CallResult<LeaderboardFindResult_t> m_callResultFindLeaderboard;
-		static Callback<PersonaStateChange_t> m_PersonaStateChange = Callback<PersonaStateChange_t>.Create(OnPersonaStateChange);
+		static Callback<PersonaStateChange_t> m_PersonaStateChange;
 		static Callback<UserStatsReceived_t> m_UserStatsReceived;
 
+		static void InitializeCallbacks() {
+			m_NumberOfCurrentPlayers = CallResult<NumberOfCurrentPlayers_t>.Create(OnNumberOfCurrentPlayers);
+			m_callResultFindLeaderboard = CallResult<LeaderboardFindResult_t>.Create(OnFindLeaderboard);
+			m_PersonaStateChange = Callback<PersonaStateChange_t>.Create(OnPersonaStateChange);
+			m_UserStatsReceived = Callback<UserStatsReceived_t>.Create(
+				(pCallback) => {
+					Console.WriteLine("[" + UserStatsReceived_t.k_iCallback + " - UserStatsReceived] - " + pCallback.m_eResult + " -- " + pCallback.m_nGameID + " -- " + pCallback.m_steamIDUser);
+				});
+		}
+
 		static void Main(string[] args) {
-			if (!SteamAPI.Init()) {
-				Console.WriteLine("SteamAPI.Init() failed!");
+			try {
+				if (!SteamAPI.Init()) {
+					Console.WriteLine("SteamAPI.Init() failed!");
+					return;
+				}
+			}
+			catch (DllNotFoundException e) { // We check this here as it will be the first instance of it.
+				Console.WriteLine(e);
 				return;
 			}
 
@@ -23,22 +39,33 @@ namespace SteamworksNET_StandaloneTest {
 				return;
 			}
 
-			m_callResultFindLeaderboard = CallResult<LeaderboardFindResult_t>.Create(OnFindLeaderboard);
-			m_UserStatsReceived = Callback<UserStatsReceived_t>.Create(OnUserStatsReceived);
+			if (!DllCheck.Test()) {
+				Console.WriteLine("You're using the wrong dlls for this platform!");
+				return;
+			}
+
+			InitializeCallbacks(); // We do this after SteamAPI.Init() has occured
+
+
+			Console.WriteLine("Reqesting Current Stats - " + SteamUserStats.RequestCurrentStats());
 
 			Console.WriteLine("CurrentGameLanguage: " + SteamApps.GetCurrentGameLanguage());
 			Console.WriteLine("PersonaName: " + SteamFriends.GetPersonaName());
-			string folder;
-			uint length = SteamApps.GetAppInstallDir(SteamUtils.GetAppID(), out folder, 260);
-			Console.WriteLine("AppInstallDir: " + length + " " + folder);
-
-			SteamUserStats.RequestCurrentStats();
-			Console.WriteLine("Reqesting Current Stats");
 
 			{
-				SteamAPICall_t hSteamAPICall = SteamUserStats.GetNumberOfCurrentPlayers();
-				NumberOfCurrentPlayers.Set(hSteamAPICall);
-				Console.WriteLine("GetNumberOfCurrentPlayers() - " + hSteamAPICall);
+				string folder;
+				uint length = SteamApps.GetAppInstallDir(SteamUtils.GetAppID(), out folder, 260);
+				Console.WriteLine("AppInstallDir: " + length + " " + folder);
+			}
+
+
+			m_NumberOfCurrentPlayers.Set(SteamUserStats.GetNumberOfCurrentPlayers());
+			Console.WriteLine("Reqesting Number of Current Players");
+
+			{
+				SteamAPICall_t hSteamAPICall = SteamUserStats.FindLeaderboard("Quickest Win");
+				m_callResultFindLeaderboard.Set(hSteamAPICall);
+				Console.WriteLine("Requesting Leaderboard");
 			}
 
 			while (true) {
@@ -62,31 +89,27 @@ namespace SteamworksNET_StandaloneTest {
 					}
 					else if (info.Key == ConsoleKey.D2) {
 						SteamAPICall_t hSteamAPICall = SteamUserStats.GetNumberOfCurrentPlayers();
-						NumberOfCurrentPlayers.Set(hSteamAPICall);
+						m_NumberOfCurrentPlayers.Set(hSteamAPICall);
 						Console.WriteLine("GetNumberOfCurrentPlayers() - " + hSteamAPICall);
 					}
 				}
 
 				Thread.Sleep(50);
 			}
-
 			SteamAPI.Shutdown();
 		}
 
+		
+		static void OnNumberOfCurrentPlayers(NumberOfCurrentPlayers_t pCallback, bool bIOFailure) {
+			Console.WriteLine("[" + NumberOfCurrentPlayers_t.k_iCallback + " - NumberOfCurrentPlayers] - " + pCallback.m_bSuccess + " -- " + pCallback.m_cPlayers);
+		}
+		
 		static void OnFindLeaderboard(LeaderboardFindResult_t pCallback, bool bIOFailure) {
 			Console.WriteLine("[" + LeaderboardFindResult_t.k_iCallback + " - LeaderboardFindResult] - " + pCallback.m_bLeaderboardFound + " -- " + pCallback.m_hSteamLeaderboard);
 		}
-
-		static void OnUserStatsReceived(UserStatsReceived_t pCallback) {
-			Console.WriteLine("[" + UserStatsReceived_t.k_iCallback + " - UserStatsReceived] - " + pCallback.m_eResult + " -- " + pCallback.m_nGameID + " -- " + pCallback.m_steamIDUser);
-		}
-
+		
 		static void OnPersonaStateChange(PersonaStateChange_t pCallback) {
 			Console.WriteLine("[" + PersonaStateChange_t.k_iCallback + " - PersonaStateChange] - " + pCallback.m_ulSteamID + " -- " + pCallback.m_nChangeFlags);
-		}
-
-		static void OnNumberOfCurrentPlayers(NumberOfCurrentPlayers_t pCallback, bool bIOFailure) {
-			Console.WriteLine("[" + NumberOfCurrentPlayers_t.k_iCallback + " - NumberOfCurrentPlayers] - " + pCallback.m_bSuccess + " -- " + pCallback.m_cPlayers);
 		}
 	}
 }
